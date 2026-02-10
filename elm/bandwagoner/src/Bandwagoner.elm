1eebeea18890abded71c83d480d3505a0780341e
Arnaud Pithon <arnaud@pithon.dev>
2026-02-11 00:28:11 +0100

    feat(elm/bandwagoner): implémentation

--- elm/bandwagoner/src/Bandwagoner.elm
+++ elm/bandwagoner/src/Bandwagoner.elm
@@ -1,22 +1,39 @@
 module Bandwagoner exposing (..)
 
--- TODO: please define a type alias for the 'Coach' record
--- TODO: please define the 'Stats' record
--- TODO: please define the 'Team' record
+
+type alias Coach =
+    { name : String
+    , formerPlayer : Bool
+    }
 
 
+type alias Stats =
+    { wins : Int
+    , losses : Int
+    }
+
+
+type alias Team =
+    { name : String
+    , coach : Coach
+    , stats : Stats
+    }
+
+
+replaceCoach : Coach -> Team -> Team
 replaceCoach newCoach team =
-    Debug.todo "Please implement this function"
+    { team | coach = newCoach }
 
 
+createTeam : String -> Stats -> Coach -> Team
 createTeam name stats coach =
-    Debug.todo "Please implement this function"
+    Team name coach stats
 
 
-
--- TODO: use an extensible record to accept any record that has a `stats: Stats` field
--- TODO: use pattern matching in the function parameter to get the `stats` field from the record
-
-
-rootForTeam team =
-    Debug.todo "Please implement this function, try to pattern match in the function parameter"
+rootForTeam : { a | stats : Stats } -> Bool
+rootForTeam { stats } =
+    let
+        { wins, losses } =
+            stats
+    in
+    wins > losses
