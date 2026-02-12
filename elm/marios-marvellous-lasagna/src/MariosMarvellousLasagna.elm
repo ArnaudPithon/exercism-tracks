6f559896b0b67edf9f207ea06c82fa16d7cd5c09
Arnaud Pithon <arnaud@pithon.dev>
2026-02-12 17:48:18 +0100

    feat(elm/marios-marvellous-lasagna): implémentation

--- elm/marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
+++ elm/marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
@@ -1,3 +1,15 @@
 module MariosMarvellousLasagna exposing (remainingTimeInMinutes)
 
--- TODO: define the remainingTimeInMinutes function
+
+remainingTimeInMinutes : Int -> Int -> Int
+remainingTimeInMinutes layers elapsedTime =
+    let
+        expectedMinutesInOven =
+            40
+
+        preparationTimeInMinutes =
+            layers * 2
+    in
+    preparationTimeInMinutes
+        + expectedMinutesInOven
+        - elapsedTime
