4fa67a5863fd2353b6550b91c4237ca7eefb9994
Arnaud Pithon <arnaud@pithon.dev>
2026-02-12 22:47:08 +0100

    feat(elm/magician-in-training): implémentation

--- elm/magician-in-training/src/MagicianInTraining.elm
+++ elm/magician-in-training/src/MagicianInTraining.elm
@@ -1,28 +1,38 @@
 module MagicianInTraining exposing (..)
 
--- todo: import the Array module
+import Array exposing (..)
 
 
 getCard : Int -> Array Int -> Maybe Int
 getCard index deck =
-    Debug.todo "Implement getCard function"
+    get index deck
 
 
 setCard : Int -> Int -> Array Int -> Array Int
 setCard index newCard deck =
-    Debug.todo "Implement setCard function"
+    set index newCard deck
 
 
 addCard : Int -> Array Int -> Array Int
 addCard newCard deck =
-    Debug.todo "Implement addCard function"
+    push newCard deck
 
 
 removeCard : Int -> Array Int -> Array Int
 removeCard index deck =
-    Debug.todo "Implement removeCard function"
+    if index >= length deck then
+        deck
+
+    else
+        append
+            (slice 0 index deck)
+            (slice (index + 1) (length deck) deck)
 
 
 evenCardCount : Array Int -> Int
 evenCardCount deck =
-    Debug.todo "Implement evenCardCount function"
+    let
+        isEven n =
+            modBy 2 n == 0
+    in
+    length (filter isEven deck)
