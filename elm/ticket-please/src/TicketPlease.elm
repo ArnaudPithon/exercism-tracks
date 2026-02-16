c614d3299a5e66d08fafcf13e095b00e15ff6300
Arnaud Pithon <arnaud@pithon.dev>
2026-02-17 00:49:29 +0100

    feat(elm/ticket-please): implémentation

--- elm/ticket-please/src/TicketPlease.elm
+++ elm/ticket-please/src/TicketPlease.elm
@@ -1,23 +1,44 @@
 module TicketPlease exposing (..)
 
+import List
 import TicketPleaseSupport exposing (Status(..), Ticket(..), User(..))
 
 
 emptyComment : ( User, String ) -> Bool
-emptyComment =
-    Debug.todo "Please implement the function emptyComment"
+emptyComment ( _, comment ) =
+    comment == ""
 
 
 numberOfCreatorComments : Ticket -> Int
-numberOfCreatorComments =
-    Debug.todo "Please implement the function numberOfCreatorComments"
+numberOfCreatorComments (Ticket { createdBy, comments }) =
+    let
+        ( creator, _ ) =
+            createdBy
+
+        count pattern list =
+            List.filter pattern list |> List.length
+    in
+    count (\( user, _ ) -> user == creator) comments
 
 
 assignedToDevTeam : Ticket -> Bool
-assignedToDevTeam =
-    Debug.todo "Please implement the function assignedToDevTeam"
+assignedToDevTeam (Ticket { assignedTo }) =
+    case assignedTo of
+        Just (User name) ->
+            List.member name [ "Alice", "Bob", "Charlie" ]
+
+        Nothing ->
+            False
 
 
 assignTicketTo : User -> Ticket -> Ticket
-assignTicketTo =
-    Debug.todo "Please implement the function assignTicketTo"
+assignTicketTo user (Ticket ({ status } as ticket)) =
+    case status of
+        Archived ->
+            Ticket ticket
+
+        New ->
+            Ticket { ticket | assignedTo = Just user, status = InProgress }
+
+        _ ->
+            Ticket { ticket | assignedTo = Just user }
