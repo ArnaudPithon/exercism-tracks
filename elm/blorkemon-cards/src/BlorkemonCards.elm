6a9619956434b1adbf60d0beff2e8fa3de9269d8
Arnaud Pithon <arnaud@pithon.dev>
2026-02-20 00:40:38 +0100

    feat(elm/blorkemon-cards): implémentation

--- elm/blorkemon-cards/src/BlorkemonCards.elm
+++ elm/blorkemon-cards/src/BlorkemonCards.elm
@@ -15,29 +15,71 @@ type alias Card =
 
 isMorePowerful : Card -> Card -> Bool
 isMorePowerful card1 card2 =
-    Debug.todo "Please implement this function"
+    card1.power > card2.power
 
 
 maxPower : Card -> Card -> Int
 maxPower card1 card2 =
-    Debug.todo "Please implement this function"
+    max card1.power card2.power
 
 
 sortByMonsterName : List Card -> List Card
 sortByMonsterName cards =
-    Debug.todo "Please implement this function"
+    List.sortBy .monster cards
 
 
 sortByCoolness : List Card -> List Card
 sortByCoolness cards =
-    Debug.todo "Please implement this function"
+    let
+        shiniesSorted =
+            List.filter (\c -> c.shiny) cards
+                |> List.sortBy .power
+                |> List.reverse
+
+        otherSorted =
+            List.filter (\c -> not c.shiny) cards
+                |> List.sortBy .power
+                |> List.reverse
+    in
+    List.append shiniesSorted otherSorted
 
 
 compareShinyPower : Card -> Card -> Order
 compareShinyPower card1 card2 =
-    Debug.todo "Please implement this function"
+    let
+        compareShiny =
+            case ( card1.shiny, card2.shiny ) of
+                ( True, False ) ->
+                    GT
+
+                ( False, True ) ->
+                    LT
+
+                _ ->
+                    EQ
+
+        comparePower =
+            if isMorePowerful card1 card2 then
+                GT
+
+            else
+                LT
+    in
+    if card1.power == card2.power then
+        compareShiny
+
+    else
+        comparePower
 
 
 expectedWinner : Card -> Card -> String
 expectedWinner card1 card2 =
-    Debug.todo "Please implement this function"
+    case compareShinyPower card1 card2 of
+        GT ->
+            card1.monster
+
+        LT ->
+            card2.monster
+
+        EQ ->
+            "too close to call"
