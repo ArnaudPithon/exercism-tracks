610a550df81cac7d1f60e48509c4af5fbaa62d8f
Arnaud Pithon <arnaud@pithon.dev>
2026-02-19 04:22:51 +0100

    feat(elm/paolas-prestigious-pizza): implémentation

--- elm/paolas-prestigious-pizza/src/PaolasPrestigiousPizza.elm
+++ elm/paolas-prestigious-pizza/src/PaolasPrestigiousPizza.elm
@@ -22,34 +22,86 @@ type alias Pizza =
 
 priceParser : Parser Int
 priceParser =
-    Parser.problem "Please implement this parser"
+    Parser.succeed identity
+        |= Parser.int
+        |. Parser.symbol "€"
 
 
 vegetarianParser : Parser Bool
 vegetarianParser =
-    Parser.problem "Please implement this parser"
+    Parser.oneOf
+        [ Parser.succeed True
+            |. Parser.keyword "(v)"
+        , Parser.succeed False
+        ]
 
 
 wordParser : Parser String
 wordParser =
-    Parser.problem "Please implement this parser"
+    Parser.chompWhile Char.isAlpha
+        |> Parser.getChompedString
+        |> Parser.map String.toLower
 
 
 ingredientsParser : Parser (List String)
 ingredientsParser =
-    Parser.problem "Please implement this parser"
+    Parser.sequence
+        { start = ""
+        , separator = ","
+        , end = ""
+        , spaces = Parser.spaces
+        , item = oneIngredientParser
+        , trailing = Parser.Optional
+        }
+
+
+
+{-
+   Regina: tomato, ham, mushrooms, cantal - 11€
+   Formaggio (v): tomato, emmental - 8€
+-}
 
 
 pizzaParser : Parser Pizza
 pizzaParser =
-    Parser.problem "Please implement this parser"
+    Parser.succeed Pizza
+        |= wordParser
+        |. Parser.spaces
+        |= vegetarianParser
+        |. Parser.symbol ":"
+        |. Parser.spaces
+        |= ingredientsParser
+        |. Parser.spaces
+        |. Parser.symbol "-"
+        |. Parser.spaces
+        |= priceParser
 
 
 menuParser : Parser (List Pizza)
 menuParser =
-    Parser.problem "Please implement this parser"
+    Parser.sequence
+        { start = ""
+        , separator = "\n"
+        , end = ""
+        , spaces = Parser.succeed ()
+        , item = pizzaParser
+        , trailing = Parser.Optional
+        }
+        |. Parser.end
 
 
 oneIngredientParser : Parser String
 oneIngredientParser =
-    Parser.problem "Please implement this parser"
+    Parser.chompWhile (\c -> Char.isAlpha c || c == ' ')
+        |> Parser.getChompedString
+        |> Parser.map String.toLower
+        |> Parser.map String.trim
+        |> Parser.andThen
+            (\ingredient ->
+                case ingredient of
+                    "" ->
+                        Parser.problem "empty string"
+
+                    _ ->
+                        Parser.succeed ingredient
+            )
