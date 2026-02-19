f41d3c23c567e0bf82231e292035a26b1a93ef40
Arnaud Pithon <arnaud@pithon.dev>
2026-02-19 15:13:34 +0100

    feat(elm/paulas-palindromes): implémentation

--- elm/paulas-palindromes/src/Main.elm
+++ elm/paulas-palindromes/src/Main.elm
@@ -7,34 +7,29 @@ import Html.Events exposing (onInput)
 
 
 
--- When you have finished this exercise, this file will be an Elm program.
---
--- You can copy this file to Ellie (an online Elm sandbox - https://ellie-app.com/)
--- to see it running and interact with it.
---
--- If you are solving the exercise locally (and not through the exercism online editor),
--- you can additionally run it locally:
--- - `rm src/Browser.elm` (delete a fake file that we need for the online editor),
--- - `elm install elm/browser` (install the real version of the fake)
--- - `npx elm-watch@beta hot` (compile the code, hot reload and serve)
--- - Then go to the url in the output to view the application (localhost://53529 or similar)
 -- MAIN
 
 
+main : Html Msg
 main =
-    text "TODO: please implement main"
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 
 -- MODEL
 
 
-type Model
-    = TODO
+type alias Model =
+    { content : String }
 
 
+init : Model
 init =
-    Debug.todo "Implement init function"
+    { content = "" }
 
 
 
@@ -42,17 +37,36 @@ init =
 
 
 type Msg
-    = TODO
+    = Change String
 
 
+update : Msg -> Model -> Model
 update msg model =
-    -- TODO: replace this stub implementation of `update`
-    model
+    case msg of
+        Change newText ->
+            { model | content = newText }
 
 
 
 -- VIEW
 
 
+isPalindrome : String -> Bool
+isPalindrome content =
+    content == String.reverse content
+
+
+view : Model -> Html Msg
 view model =
-    text "TODO: please implement view"
+    let
+        message =
+            if isPalindrome model.content then
+                "This is a palindrome"
+
+            else
+                "Not a palindrome"
+    in
+    Html.div []
+        [ input [ value model.content, onInput Change ] []
+        , div [] [ text message ]
+        ]
