84aea26742f7eeddb78a8840ed2dc8c405663aa4
Arnaud Pithon <arnaud@pithon.dev>
2026-02-27 22:22:17 +0100

    feat(elm/gotta-snatch-em-all): implémentation

--- elm/gotta-snatch-em-all/src/GottaSnatchEmAll.elm
+++ elm/gotta-snatch-em-all/src/GottaSnatchEmAll.elm
@@ -1,5 +1,6 @@
 module GottaSnatchEmAll exposing (..)
 
+import List exposing (member)
 import Set exposing (Set)
 
 
@@ -9,32 +10,48 @@ type alias Card =
 
 newCollection : Card -> Set Card
 newCollection card =
-    Debug.todo "Please implement newCollection"
+    Set.singleton card
 
 
 addCard : Card -> Set Card -> ( Bool, Set Card )
 addCard card collection =
-    Debug.todo "Please implement addCard"
+    ( Set.member card collection
+    , Set.insert card collection
+    )
 
 
 tradeCard : Card -> Card -> Set Card -> ( Bool, Set Card )
 tradeCard yourCard theirCard collection =
-    Debug.todo "Please implement tradeCard"
+    let
+        worth =
+            Set.member yourCard collection || not (Set.member theirCard collection)
+    in
+    ( worth, Set.remove yourCard collection )
 
 
 removeDuplicates : List Card -> List Card
 removeDuplicates cards =
-    Debug.todo "Please implement removeDuplicates"
+    Set.fromList cards |> Set.toList
 
 
 extraCards : Set Card -> Set Card -> Int
 extraCards yourCollection theirCollection =
-    Debug.todo "Please implement extraCards"
+    Set.diff yourCollection theirCollection
+        |> Set.size
 
 
 boringCards : List (Set Card) -> List Card
 boringCards collections =
-    Debug.todo "Please implement boringCards"
+    let
+        bore coll =
+            case coll of
+                set1 :: otherSets ->
+                    Set.intersect set1 (bore otherSets)
+
+                [] ->
+                    Set.empty
+    in
+    bore collections |> Set.toList
 
 
 totalCards : List (Set Card) -> Int
